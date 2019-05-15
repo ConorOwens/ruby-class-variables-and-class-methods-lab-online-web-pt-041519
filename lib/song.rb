a57e01d92f3f0c79da908723e0d0022013462838
Conor Owens <conorj.owens@gmail.com>
2019-05-15 03:15:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,20 @@
 class Song 
   
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
   attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    unless @@artists.include?(artist)
+      @@artists << artist
+    unless @@genre.include?(genre)
+      @@genre << genre
+    @@count += 1
   end
   
 end
